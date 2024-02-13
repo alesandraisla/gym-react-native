4b549f5d63edf9bec81e2975dab692ce1fa94aa3
AlesandraIsla <alesandramempis@gmail.com>
2024-02-13 16:57:42 -0300

    Create public routes

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import { NativeBaseProvider } from 'native-base';
 import { Loading } from './src/components/Loading';
 import { THEME } from './src/theme';
-import { SignIn } from '@screens/SignIn';
 import { SignUp } from './src/screens/SignUp';
+import { Routes } from '@routes/index';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,8 +16,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {/* {fontsLoaded ? <SignIn /> : <Loading />} */}
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {/* {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />} */}
+      {fontsLoaded ? <Routes /> : <Loading />}
 
     </NativeBaseProvider>
   );
