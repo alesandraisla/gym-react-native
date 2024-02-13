d7dfd9209028217b6d43d66df692d9d2c35ef34e
AlesandraIsla <alesandramempis@gmail.com>
2024-02-13 16:24:25 -0300

    Create component button

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,10 +2,11 @@ import { VStack, Image, Center, Text, Heading } from "native-base";
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg';
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image 
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -27,8 +28,20 @@ export function SignIn() {
         </Heading>
       </Center>
 
-      <Input placeholder="email" />
-      <Input placeholder="Senha"/>
+      <Input 
+          placeholder="E-mail" 
+          keyboardType="email-address"
+          autoCapitalize="none"
+
+        />
+        <Input 
+          placeholder="Senha" 
+          secureTextEntry
+        />
+
+    <Button title={"Acessar"} />
+
+    <Button title={"Acessar"} variant={"outline"} />
 
     </VStack>
   );
