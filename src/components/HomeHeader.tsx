f37e4aeb3641116d84851e34042840395486ec6b
AlesandraIsla <alesandramempis@gmail.com>
2024-02-17 10:07:48 -0300

    Component UserPhoto

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,35 @@
+import { TouchableOpacity } from 'react-native';
+import { Heading, HStack, Text, VStack, Icon } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+import { UserPhoto } from './UsePhoto';
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/alesandraisla.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+      <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100" fontSize="md">
+          Alesandra
+
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name="logout"
+          color="gray.200"
+          size={7}
+        />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
