23ff999fe877ed5aec286daf60b7d2358297e285
AlesandraIsla <alesandramempis@gmail.com>
2024-02-17 14:50:32 -0300

    Component Card

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,25 +1,55 @@
-import { HStack, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 import { HomeHeader } from '../components/HomeHeader';
 import { Group } from '../components/Group';
 import { useState } from 'react';
+import { ExerciseCard } from '../components/ExerciseCard';
 
 export function Home() {
-  const [groupSelected, setGroupSelected] = useState('costa')
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro']);
+  const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
+  const [groupSelected, setGroupSelected] = useState('Costas');
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-      <Group 
-          name="costa" 
-          isActive={groupSelected === 'costa'}
-          onPress={() => setGroupSelected('costa')}
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            name={item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between">
+          <Heading color="gray.200" fontSize="sm">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+        <FlatList 
+          data={exercises}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20
+          }}
         />
-        <Group 
-          name="ombro" 
-          isActive={groupSelected === 'ombro'}
-          onPress={() => setGroupSelected('ombro')}
-        />
-      </HStack>
+      </VStack>
     </VStack>
   );
 }
