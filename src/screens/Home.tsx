f6599430b58b57f37ab2d3df00763f5e5b01c2e4
AlesandraIsla <alesandramempis@gmail.com>
2024-02-17 11:31:03 -0300

    Adding colors to the group button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,25 @@
-import { VStack } from 'native-base';
+import { HStack, VStack } from 'native-base';
 import { HomeHeader } from '../components/HomeHeader';
+import { Group } from '../components/Group';
+import { useState } from 'react';
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+      <Group 
+          name="costa" 
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group 
+          name="ombro" 
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
+      </HStack>
     </VStack>
   );
 }
