a95a0be6ab8155a62e5707e4eed533665b968333
AlesandraIsla <alesandramempis@gmail.com>
2024-02-17 16:44:39 -0300

    Create back button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,11 +3,20 @@ import { HomeHeader } from '../components/HomeHeader';
 import { Group } from '../components/Group';
 import { useState } from 'react';
 import { ExerciseCard } from '../components/ExerciseCard';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '../routes/app.routes';
 
 export function Home() {
   const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro']);
   const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada unilateral', 'Levantamento terras']);
   const [groupSelected, setGroupSelected] = useState('Costas');
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
+
   return (
     <VStack flex={1}>
       <HomeHeader />
@@ -42,7 +51,7 @@ export function Home() {
           data={exercises}
           keyExtractor={item => item}
           renderItem={({ item }) => (
-            <ExerciseCard />
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
           )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{
